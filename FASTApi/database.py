cdc7a1d4962a1e5ad255c971a3ce515286372f36
Your Name <user@example.com>
2026-07-24 14:58:09 +0530

    adding docker dependencis

--- FASTApi/database.py
+++ FASTApi/database.py
@@ -4,9 +4,13 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "postgresql://postgres:postgres123@localhost:5432/rental_db"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL,
+            "postgresql://postgres:postgres123@localhost:5432/rental_db"
+)
 
 
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
